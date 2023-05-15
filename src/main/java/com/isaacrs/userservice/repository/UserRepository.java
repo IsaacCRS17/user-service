ce81da82decf327f9b15e608298b3875010ebf52
IsaacCRS17 <crs170919@gmail.com>
2023-05-15 11:06:58 -0500

    "CRUD basic"

---
+++ src/main/java/com/isaacrs/userservice/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.isaacrs.userservice.repository;
+
+import com.isaacrs.userservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
---
+++ src/main/java/com/isaacrs/userservice/service/UserService.java
@@ -0,0 +1,27 @@
+package com.isaacrs.userservice.service;
+
+import com.isaacrs.userservice.entity.User;
+import com.isaacrs.userservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAll(){
+        return userRepository.findAll();
+    }
+
+    public User getUserById(int id){
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User save(User user){
+        User userNew = userRepository.save(user);
+        return userNew;
+    }
+}
